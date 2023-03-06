c6528b10668304a6255fde0df7b17340b68c736e
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-03-06 16:45:52 +0100

    Wykorzystanie algorytmu Median z dwóch tablic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         int[] num1 = {1,2};
@@ -8,11 +6,23 @@ public class Main {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr = new int[nums1.length + nums2.length];
-        System.arraycopy(nums1, 0, arr, 0, nums1.length);
-        System.arraycopy(nums2, 0, arr, nums1.length, nums2.length);
-        Arrays.sort(arr);
-        int halfArr = arr.length/2;
-        return arr.length % 2 == 0 ? (double) (arr[halfArr] + arr[halfArr-1])/2 : arr[halfArr];
+        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+        int imin = 0;
+        int imax = nums1.length;
+        int halfLen = (nums1.length + nums2.length + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+            if (i < nums1.length && nums2[j-1] > nums1[i]) imin = i + 1;
+            else if (i > 0 && nums1[i-1] > nums2[j]) {
+                imax = i - 1;
+            } else {
+                int maxLeft = (i == 0) ? nums2[j-1] : (j == 0) ? nums1[i-1] : Math.max(nums1[i-1], nums2[j-1]);
+                if ((nums1.length + nums2.length) % 2 == 1) return maxLeft;
+                int minRight = (i == nums1.length) ? nums2[j] : (j == nums2.length) ? nums1[i] : Math.min(nums1[i], nums2[j]);
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
     }
 }
