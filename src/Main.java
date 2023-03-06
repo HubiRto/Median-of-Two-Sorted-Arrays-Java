b04f99b35f621c96c82a4c30d5e2d6cb51973af4
Hubert Rybicki <hubert.rybicki.hr@gmail.com>
2023-03-06 16:33:00 +0100

    Skrócenie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] num1 = {1,2};
+        int[] num2 = {3,4};
+        System.out.println(findMedianSortedArrays(num1, num2));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] arr = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, arr, 0, nums1.length);
+        System.arraycopy(nums2, 0, arr, nums1.length, nums2.length);
+        Arrays.sort(arr);
+        int halfArr = arr.length/2;
+        return arr.length % 2 == 0 ? (double) (arr[halfArr] + arr[halfArr-1])/2 : arr[halfArr];
     }
 }
